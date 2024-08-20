11e012bdf63f983c86014a5beb37735d109b3033
Marius Vestol <marves@stud.ntnu.no>
2024-08-20 16:40:28 +0200

    pygame

--- test1.py
+++ test1.py
@@ -1,1 +1,33 @@
 print("hello world")
+
+# code noe <- lager ny fil i vscode :()
+
+
+import pygame
+
+
+
+pygame.init()
+
+# game window
+screen_width = 800
+screen_height = 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+
+
+screen.fill((255,255,255))
+pygame.display.flip()
+
+# game loop
+
+run = True
+while run:
+	# event handler
+	# samme som while run == True
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
+pygame.quit()
+
