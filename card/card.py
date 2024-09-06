33e5eb6694724699f003be4c4d5c13ac8673bb2e
Marius Vestol <marves@stud.ntnu.no>
2024-09-06 11:01:08 +0200

    ny

---
+++ card/card.py
@@ -0,0 +1,34 @@
+import tkinter
+
+
+root = Tk()
+
+
+
+
+#sjekke hver milimeter
+
+
+green = True
+
+#pip3 install opencv-python
+
+
+def drive():
+	print("tester")
+
+
+def test():
+	if green:
+		print("yes")
+		#break ???
+
+	else:
+		print("no")
+
+
+
+
+
+
+root.mainloop()
